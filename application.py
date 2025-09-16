bb20b6ee76696a64749c7cb34258dffdd3e3e4b1
Emmanuel kufre <whitemaxwell5@gmail.com>
2025-09-16 22:06:03 +0100

    Enable Flask app to run on host and port when executed directly

--- application.py
+++ application.py
@@ -65,5 +65,5 @@ def test():
     result = utils.test_search(question)
     return jsonify(result)
 
-# if __name__ == '__main__':
-#   application.run(host='0.0.0.0', port=5000) 
+if __name__ == '__main__':
+  application.run(host='0.0.0.0', port=5000) 
