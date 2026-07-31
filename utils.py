a36e4cef2440b82214afeced7cef724def65dee5
Emmanuel kufre <whitemaxwell5@gmail.com>
2026-08-01 00:02:47 +0100

    fix: handle pysqlite3 import for SQLite compatibility in utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
 import os
-import tempfile
+import sys
+
+try:
+    import pysqlite3
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 import json
 import datetime
 import openai
