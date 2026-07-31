390d6f460602824e291fe5d54b59d449937e248a
Emmanuel kufre <whitemaxwell5@gmail.com>
2026-07-31 19:03:34 +0100

    fix: integrate pysqlite3 for SQLite support in utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import tempfile
 import json
 import datetime
 import openai
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import chromadb
 import traceback
 from chromadb.utils import embedding_functions
