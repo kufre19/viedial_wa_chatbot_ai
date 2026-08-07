f9f30700b64f8d8b46ed92a9f5815edf868cdfc8
Emmanuel kufre <whitemaxwell5@gmail.com>
2026-08-07 12:20:29 +0100

    update: change embedding source to menopause documents and remove unused testsqlite.py file

--- test.py
+++ test.py
@@ -7,4 +7,4 @@ import utils
 # utils.process_folder_and_create_embeddings("documents/contraceptives")
 # utils.process_folder_and_create_embeddings("documents/stress")
 # utils.process_folder_and_create_embeddings("documents/Polyendocrine_Metabolic_Ovarian_Syndrome ")
-utils.process_folder_and_create_embeddings("documents")
+utils.process_folder_and_create_embeddings("documents/menopause")
